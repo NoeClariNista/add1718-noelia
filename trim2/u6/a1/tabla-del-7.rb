a13a2e664c08bef8f0d305c605ef18a6d56cf5da
Noelia Hernández Domínguez <chispa1992@gmail.com>
2018-02-01 17:41:14 +0000

    tabla-del-7

--- trim2/u6/a1/tabla-del-7.rb
+++ trim2/u6/a1/tabla-del-7.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/ruby
 
 numeros = [1,2,3,4,5,6,7,8,9,10]
-numero_siete = 7
+numero_multiplicar = 7
 
 numeros.each do |numero|
-  puts "#{numero_siete} x #{numero} = #{numero_siete * numero}" 
+  puts "#{numero_multiplicar} x #{numero} = #{numero_multiplicar * numero}" 
 end
