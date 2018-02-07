9b7a2aae74aa802f461f960062b6ffd7b4d4299f
Noelia Hernandez Dominguez <chispa1992@gmail.com>
2018-02-07 12:46:19 +0000

    Final

--- trim2/u6/a2/cal2.rb
+++ trim2/u6/a2/cal2.rb
@@ -1,17 +1,17 @@
 #!/usr/bin/ruby
 
-fichero = ARGV[0]
+file = ARGV[0]
 
 if ARGV.size != 1
   puts "Te Faltan O Te Sobran Argumentos."
 else
-  contenido = `cat #{fichero}`
-  lineas = contenido.split("\n")
-  lineas.each do |linea|
-    campos = linea.split()
- 	num1 = campos[0].to_i
-    op = campos[1]
-    num2 = campos[2].to_i
+  content = `cat #{file}`
+  lines = content.split("\n")
+  lines.each do |line|
+    field = line.split()
+ 	num1 = field[0].to_i
+    op = field[1]
+    num2 = field[2].to_i
     if op == "+"
       puts "#{num1} #{op} #{num2} = #{num1 + num2}"
     elsif op == "-"
