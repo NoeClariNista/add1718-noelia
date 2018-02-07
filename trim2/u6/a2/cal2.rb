af1c331b1ba7251f9f5c11994a57fa66d2704a72
Noelia Hernandez Dominguez <chispa1992@gmail.com>
2018-02-07 13:01:37 +0000

    Final

--- trim2/u6/a2/cal2.rb
+++ trim2/u6/a2/cal2.rb
@@ -9,7 +9,7 @@ else
   lines = content.split("\n")
   lines.each do |line|
     field = line.split()
- 	num1 = field[0].to_i
+    num1 = field[0].to_i
     op = field[1]
     num2 = field[2].to_i
     if op == "+"
