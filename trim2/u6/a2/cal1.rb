3c4549ae7da330b6cfd2c49f1b12aac0ddb0cdab
Noelia Hernandez Dominguez <chispa1992@gmail.com>
2018-02-07 12:36:54 +0000

    Final

--- trim2/u6/a2/cal1.rb
+++ trim2/u6/a2/cal1.rb
@@ -4,17 +4,17 @@ num1 = ARGV[0].to_i
 op = ARGV[1]
 num2 = ARGV[2].to_i
 
-if ARGV.size != 3 # revisar
-  puts "Te Falta O Te Sobran Argumentos."
+if ARGV.size != 3
+  puts "Te Faltan O Te Sobran Argumentos."
 else
   if op == "+"
     puts "#{num1} #{op} #{num2} = #{num1 + num2}"
   elsif op == "-"
     puts "#{num1} #{op} #{num2} = #{num1 - num2}"
   elsif op == "x"
-    puts "#{num1} #{op} #{num2} = #{num1 * num2}"  
+    puts "#{num1} #{op} #{num2} = #{num1 * num2}"
   elsif op == "/"
-    puts "#{num1} #{op} #{num2} = #{num1 / num2}"  
+    puts "#{num1} #{op} #{num2} = #{num1 / num2}"
   else
     puts "ERROR."
   end
